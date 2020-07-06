251099d1c70768fbd85c0aa80f22f8743c6a3a17
Ragnarok54 <bogdanzanfir23@gmail.com>
2020-07-06 22:51:27 +0300

    Small behavioural changes to UI
    Also changed few comments and definitions. Main change is the Combo Box content changes based on the first Combo Box

--- src/application/main.java
+++ src/application/main.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
@@ -13,6 +12,7 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -47,42 +47,53 @@ public class main {
 		log.info("Program started");
 
 		// Create the initial frame
-		JFrame initialFrame = new JFrame("Start");
-		
+		JFrame initialFrame = new JFrame("File Converter");
+
+		// Text for the initial frame
+		JLabel mainText = new JLabel("Main Menu");
+		mainText.setBounds(110, 30, 180, 40);
+		mainText.setFont(mainText.getFont().deriveFont(30.0f));
+
+		// mainText.set
 		// Create a button for generating files
-		JButton generateButton = new JButton("Generate");
-		generateButton.setBounds(140, 100, 95, 30);
-		
+		JButton generateButton = new JButton("Generator");
+		generateButton.setBounds(140, 130, 95, 30);
+
 		// Create a button for converting
-		JButton convertButton = new JButton("Convert");
-		convertButton.setBounds(140, 150, 95, 30);
-	
+		JButton convertButton = new JButton("Convertor");
+		convertButton.setBounds(140, 180, 95, 30);
+
 		// Create a exit button
 		JButton exitButton = new JButton("Exit");
-		exitButton.setBounds(140, 200, 95, 30);
+		exitButton.setBounds(140, 230, 95, 30);
 
 		// Action for the generate button
 		generateButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Set the initial frame as invisible
 				initialFrame.setVisible(false);
-				
+
 				FileGenerator generator = new FileGenerator();
-				
+
 				// Create a new frame
-				JFrame generateFrame = new JFrame("Generate Frame");
-				
+				JFrame generateFrame = new JFrame("Generate Input Files");
+
+				// Text for the text generation frame
+				JLabel generateText = new JLabel("Choose an option");
+				generateText.setBounds(115, 30, 180, 40);
+				generateText.setFont(generateText.getFont().deriveFont(20.0f));
+
 				// Create a button for generating random tests
 				JButton randomButton = new JButton("Random tests");
-				randomButton.setBounds(125, 100, 150, 30);
-				
+				randomButton.setBounds(125, 130, 150, 30);
+
 				// Create a button for copying files from Cache folder
 				JButton premadeButton = new JButton("Premade tests");
-				premadeButton.setBounds(125, 150, 150, 30);
-				
+				premadeButton.setBounds(125, 180, 150, 30);
+
 				// Create a button for returning to initial frame
 				JButton backButton = new JButton("Back");
-				backButton.setBounds(125, 200, 150, 30);
+				backButton.setBounds(125, 230, 150, 30);
 
 				// Actions for random button
 				randomButton.addActionListener(new ActionListener() {
@@ -91,21 +102,21 @@ public class main {
 						generator.clearFolders();
 						// Generate ten or less text files
 						generator.generateTxtFiles(10);
-						// Generate an xml file with ten or less nodes
+						// Generate an XML file with ten or less nodes
 						generator.generateXmlFile(10);
 					}
 				});
-				
-				// Actions for premade button 
+
+				// Actions for premade button
 				premadeButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						// Clear the Input and the Output folders
 						generator.clearFolders();
-						// Copy the files for Cache to Input
+						// Copy the files from Cache to Input
 						generator.copyFiles();
 					}
 				});
-				
+
 				// Actions for back button
 				backButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -117,111 +128,157 @@ public class main {
 				});
 
 				// Add the buttons to the frame
+				generateFrame.add(generateText);
 				generateFrame.add(randomButton);
 				generateFrame.add(premadeButton);
 				generateFrame.add(backButton);
-				
+
 				generateFrame.setSize(400, 400);
 				generateFrame.setLayout(null);
 				generateFrame.setVisible(true);
 			}
 		});
-		
+
 		// Actions for convert button
 		convertButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Set the initial frame as invisible
 				initialFrame.setVisible(false);
-				
-				XmlManager xml = new XmlManager();
-				JsonManager json = new JsonManager();
-				ExcelManager excel = new ExcelManager();
-				PdfManager pdf = new PdfManager();
-				
+				XmlManager xmlManager = new XmlManager();
+				JsonManager jsonManager = new JsonManager();
+				ExcelManager excelManager = new ExcelManager();
+				PdfManager pdfManager = new PdfManager();
+
 				// Create a new frame for convert actions
-				JFrame convertFrame = new JFrame("Convert Frame");
-				
-				// Convert from text field
-				JTextField fromText = new JTextField("Convert from:");
-				fromText.setBounds(50, 50, 80, 20);
-				
-				// Convert to text field
-				JTextField toText = new JTextField("Convert to:");
-				toText.setBounds(250, 50, 80, 20);
-				
-				String[] from = {"Text", "XML"}; 
+				JFrame convertFrame = new JFrame("Convert");
+
+				// Convert from text label
+				JLabel fromText = new JLabel("Convert from:");
+				fromText.setBounds(45, 50, 110, 20);
+				fromText.setFont(fromText.getFont().deriveFont(15.0f));
+
+				// Convert to text label
+				JLabel toText = new JLabel("Convert to:");
+				toText.setBounds(250, 50, 100, 20);
+				toText.setFont(toText.getFont().deriveFont(15.0f));
+
+				String[] from = { "Text", "XML" };
 				// Drop down list for convert from options
-				JComboBox cbFrom = new JComboBox(from);
-				cbFrom.setBounds(50, 70,90,20);
-				
-				String[] to = {"Text", "XML", "Json", "Pdf", "Excel"};
+				JComboBox<String> cbFrom = new JComboBox<String>(from);
+				cbFrom.setBounds(50, 70, 90, 20);
+
+				// Convert TXT to the following
+				String[] txtTo = { "XML", "Json", "Pdf", "Excel" };
+				// Convert XML to the following
+				String[] xmlTo = { "Text", "Pdf", "Json" };
 				// Drop down list for convert to options
-				JComboBox cbTo = new JComboBox(to);
+				JComboBox<String> cbTo = new JComboBox<String>(txtTo);
 				cbTo.setBounds(250, 70, 90, 20);
+
+				// Label for conversion completed
+				JLabel conversionText = new JLabel("Conversion completed");
+				conversionText.setBounds(100, 300, 200, 30);
+				// Only set it visible after a conversion
+				conversionText.setVisible(false);
 				
-				// Convert button
-				JButton convert = new JButton("Convert");
-				convert.setBounds(120, 200, 150, 30);
-				// Actions for convert button
-				convert.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						if(cbFrom.getItemAt(cbFrom.getSelectedIndex()) == "Text") {
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Text") {
-								JTextField text = new JTextField("Can not convert from Text to Text!");          
-						    	text.setBounds(100, 300, 200, 30);   
-						    	convertFrame.add(text);
+				/*
+				 * Action listener for the second combo box, so 
+				 * we change its contents based on the contents
+				 * of the first combo box
+				 */
+				cbFrom.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent event) {
+						// If XML is selected
+						if (cbFrom.getItemAt(cbFrom.getSelectedIndex()) == "XML") {
+							// Remove all items
+							cbTo.removeAllItems();
+							// Add only the possible conversions
+							for (String file : xmlTo) {
+								cbTo.addItem(file);
 							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "XML") {
-								xml.convertToXml("TxtToXml");
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Json") {
-								json.convertToJson("TxtToJson");
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Pdf") {
-								pdf.convertToPdf("TxtToPdf");
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Excel") {
-								excel.convertToExcel("TxtToExcel");
-							}
-						}
-						
-						if(cbFrom.getItemAt(cbFrom.getSelectedIndex()) == "XML") {
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Text") {
-								xml.convertToTxt("file");
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "XML") {
-								JTextField text = new JTextField("Can not convert from XML to XML!");          
-						    	text.setBounds(100, 300, 200, 30);   
-						    	convertFrame.add(text);
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Json") {
-								JTextField text = new JTextField("Can not convert from XML to Json!");          
-						    	text.setBounds(100, 300, 200, 30);   
-						    	convertFrame.add(text);
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Pdf") {
-								xml.convertToPdf("file");
-							}
-							
-							if(cbTo.getItemAt(cbTo.getSelectedIndex()) == "Excel") {
-								xml.convertToExcel("file");
-							}
-							
+							// Hide "Conversion completed" text
+							conversionText.setVisible(false);
+						} else {
+							// If TEXT is selected
+							// Make sure the list is empty
+							cbTo.removeAllItems();
+							// Add only the possible conversions
+							for (String file : txtTo)
+								cbTo.addItem(file);
+							// Hide "Conversion completed" text
+							conversionText.setVisible(false);
 						}
 					}
 				});
+
+				// Convert button
+				JButton convert = new JButton("Convert");
+				convert.setBounds(120, 200, 150, 30);
 				
-				// Exit button
+				// Actions for convert button
+				convert.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (cbFrom.getItemAt(cbFrom.getSelectedIndex()) == "Text") {
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Text") {
+								JTextField text = new JTextField("Can not convert from Text to Text!");
+								text.setBounds(100, 300, 200, 30);
+								convertFrame.add(text);
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "XML") {
+								xmlManager.convertToXml("TxtToXml");
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Json") {
+								jsonManager.convertToJson("TxtToJson");
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Pdf") {
+								pdfManager.convertToPdf("TxtToPdf");
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Excel") {
+								excelManager.convertToExcel("TxtToExcel");
+							}
+						}
+
+						if (cbFrom.getItemAt(cbFrom.getSelectedIndex()) == "XML") {
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Text") {
+								xmlManager.convertToTxt("file");
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "XML") {
+								JTextField text = new JTextField("Can not convert from XML to XML!");
+								text.setBounds(100, 300, 200, 30);
+								convertFrame.add(text);
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Json") {
+								JTextField text = new JTextField("Can not convert from XML to Json!");
+								text.setBounds(100, 300, 200, 30);
+								convertFrame.add(text);
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Pdf") {
+								xmlManager.convertToPdf("file");
+							}
+
+							if (cbTo.getItemAt(cbTo.getSelectedIndex()) == "Excel") {
+								xmlManager.convertToExcel("file");
+							}
+
+						}
+						// Conversion completed, set text visible
+						conversionText.setVisible(true);
+						
+					}
+				});
+
+				// Back button
 				JButton back = new JButton("Back");
 				back.setBounds(120, 250, 150, 30);
-				// Action for exit button
+				// Action for back button
 				back.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						// Set the current frame as invisible
@@ -230,7 +287,7 @@ public class main {
 						initialFrame.setVisible(true);
 					}
 				});
-		
+
 				// Add the components to the frame
 				convertFrame.add(fromText);
 				convertFrame.add(toText);
@@ -238,13 +295,14 @@ public class main {
 				convertFrame.add(convert);
 				convertFrame.add(cbTo);
 				convertFrame.add(cbFrom);
-				
+				convertFrame.add(conversionText);
+
 				convertFrame.setLayout(null);
 				convertFrame.setSize(400, 400);
 				convertFrame.setVisible(true);
 			}
 		});
-		
+
 		// Action for exit button
 		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -253,10 +311,11 @@ public class main {
 		});
 
 		// Add the buttons to the frame
+		initialFrame.add(mainText);
 		initialFrame.add(generateButton);
 		initialFrame.add(convertButton);
 		initialFrame.add(exitButton);
-		
+
 		initialFrame.setSize(400, 400);
 		initialFrame.setLayout(null);
 		initialFrame.setVisible(true);
